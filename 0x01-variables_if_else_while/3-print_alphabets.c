b8d84a46ffb1e34108658fec4ce68725ff016067
aziddineelm <azedine160@gmail.com>
2023-03-17 16:14:48 -0700

    alphaBETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* 
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
@@ -20,9 +20,9 @@ int main(void)
 		}
 	while
 		(d <= 'Z') {
-                        putchar(d);
-                        d++;
-                }
+			putchar(d);
+			d++;
+		}
 	putchar('\n');
 	return (0);
 }
