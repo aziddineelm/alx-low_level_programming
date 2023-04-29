3c01f7d2e38a62c29392e9911899311850837042
aziddineelm <azedine160@gmail.com>
2023-04-29 13:13:13 +0000

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: Pointer to pointer to the first element of the list.
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ * @head: double pointer to the head of the list
  *
- * Return: Nothing.
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
