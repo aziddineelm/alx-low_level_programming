73ef974645eb516011634062655ca374c1284131
aziddineelm <azedine160@gmail.com>
2023-04-30 15:26:40 +0100

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,18 +8,14 @@
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *temp;
+	int sum = 0;
+	listint_t *temp = head;
 
-    if (head == NULL)
-        return (0);
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
 
-    temp = head;
-    while (temp != NULL)
-    {
-        sum += temp->n;
-        temp = temp->next;
-    }
-
-    return (sum);
+	return (sum);
 }
